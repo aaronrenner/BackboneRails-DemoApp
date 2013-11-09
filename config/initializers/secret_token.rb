09c53a7f3ba5d5ec36a4249e5fac3249b9750410
Aaron Renner <aaron@animascodelabs.com>
2013-11-09 11:20:44 -0700

    Initial setup and footer

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BackbonerailsDemo::Application.config.secret_key_base = '4c80ef18f50d4b307a248bdeed4d4850edeebd846d2e75659baf98edf1892d6ac431beeb49b7d03753663fcb9c9e2140853376b75be89c916b2d7725e3e6f858'
